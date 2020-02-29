658164ff98f75e83fcf7851790190377b75be3bb
David Auer <dreua@posteo.de>
2020-02-29 11:31:26 +0100

    MediaBox Test OK

--- pdfarranger/pikepdf_duplicate_test.py
+++ pdfarranger/pikepdf_duplicate_test.py
@@ -12,6 +12,7 @@ for i in range(4):
     pdf_output.pages.append(pdf_output.copy_foreign(pdf_input.pages[0]))
 
 for i in [0,2]:
-    pdf_output.pages[i].Rotate = 180
+    #pdf_output.pages[i].Rotate = 180
+    pdf_output.pages[i].MediaBox = [0, 0, 10, 10]
 
 pdf_output.save("duplicate_pdf_test.pdf")
